c44bb19981f0c60ab3039468fd69b6caff24457c
Tobi Kasaba <oluwatobikasaba12@gmail.com>
2025-02-03 12:53:40 +0000

    writing test in django

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """
+    Add two numbers
+    :param x:
+    :param y:
+    :return:
+    """
+    return x + y
---
+++ app/app/tests.py
@@ -0,0 +1,18 @@
+"""sample tests"""
+
+
+from django.test import SimpleTestCase
+
+from . import calc
+
+class CalcTest(SimpleTestCase):
+    """
+    Test the calc module.
+    """
+    def test_add_numbers(self):
+        """
+        Test adding two numbers.
+        :return:
+        """
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
