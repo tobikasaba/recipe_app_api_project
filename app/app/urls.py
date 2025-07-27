a24f9175b7fac069a46d41df83c335955d69dd1a
Tobi Kasaba <oluwatobikasaba12@gmail.com>
2025-07-27 20:20:15 +0100

    feat: Add health check API with tests
    - Implemented `health_check` view to return successful health status.
    - Added corresponding unit tests to validate health check endpoint functionality.
    - Registered `health-check` endpoint in application URLs.

--- app/app/urls.py
+++ app/app/urls.py
@@ -19,10 +19,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from core import views as core_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     # URL for the OpenAPI schema (used for generating API documentation and returns JSON)
+    path("api/health-check", core_views.health_check, name="health-check"),
     path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
     # URL for the Swagger UI, allowing interaction with the API documentation
     path(
